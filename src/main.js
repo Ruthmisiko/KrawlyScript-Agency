fcc742dbfe2be32585a4d6fdcc403224107efeeb
unknown <ruthnmisiko@students.seku.ac.ke>
2025-02-06 17:15:50 +0300

    added social media links

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import './index.css';
 import "./assets/css/main.css";
 import VueSmoothScroll from 'vue3-smooth-scroll';
 import router from './router'; 
+import 'font-awesome/css/font-awesome.min.css';
 
 
 const app = createApp(App)
